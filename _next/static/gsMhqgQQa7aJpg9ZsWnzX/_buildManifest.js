d8c18379c902d3e90b5b45801c10ea3aff939f9b
PierreBerger <PierreBerger@users.noreply.github.com>
2021-03-20 23:09:07 +0000

    deploy: 2e92adb812448526cf761dc87e2fd9303fc33b35

---
+++ _next/static/gsMhqgQQa7aJpg9ZsWnzX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fff89d8a5c00f71ee1637.css","static\u002Fchunks\u002Fpages\u002Findex-d090117fee7c61bbee88.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-956fa047c136933a017d.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
