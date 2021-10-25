cbad754325684d18ab4d2332c780b1ccc3d92f21
PierreBerger <PierreBerger@users.noreply.github.com>
2021-10-25 17:23:53 +0000

    deploy: 41bb559a982664738132f05e9ddaae4904b4f6c5

--- _next/static/KG0FM1U9AW30okIFrl9SN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/9b0e16f5660b4055e96f.css","static/chunks/pages/index-a146c536cd22089ce6b4.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
