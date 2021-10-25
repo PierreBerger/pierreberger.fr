d8c5fdb9a8a37c9d5ebf95b123f31a183fc92512
Pierre Berger <pierrelinkin@yahoo.fr>
2021-10-25 18:14:59 +0200

    update next

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,3 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json',
-  },
-  plugins: ['@typescript-eslint', 'react-hooks'],
-  extends: ['airbnb-typescript', 'eslint:recommended', 'plugin:react-hooks/recommended', 'prettier'],
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    'jsx-a11y/anchor-is-valid': 'off',
-    'react/jsx-props-no-spreading': 'off',
-  },
-  globals: {
-    document: false,
-    window: false,
-  },
+  extends: 'next',
 };
