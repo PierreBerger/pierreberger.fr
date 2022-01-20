955d3d44c81f473cbdf0ca97413e75e841171649
Pierre Berger <pierre.berger@wizbii.com>
2022-01-20 17:41:12 +0100

    stop using next-react-svg

--- next.config.js
+++ next.config.js
@@ -1,25 +1,15 @@
-const withReactSvg = require('next-react-svg');
-const path = require('path');
-
-module.exports = withReactSvg(
-  {
-    include: path.resolve(__dirname, 'icons'),
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/_next/:path*',
+        headers: [
+          {
+            key: 'Cache-control',
+            value: 'public, max-age=3600, must-revalidate',
+          },
+        ],
+      },
+    ];
   },
-  {
-    distDir: 'dist',
-    crossOrigin: 'anonymous',
-    async headers() {
-      return [
-        {
-          source: '/_next/:path*',
-          headers: [
-            {
-              key: 'Cache-control',
-              value: 'public, max-age=3600, must-revalidate',
-            },
-          ],
-        },
-      ];
-    },
-  }
-);
+};
