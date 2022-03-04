510ff9447bf75ee82d48e774198823e9f8332380
PierreBerger <PierreBerger@users.noreply.github.com>
2022-03-04 09:02:34 +0000

    deploy: 5a98cabe1c21988f148fed630ff8e22c2e8e7692

--- _next/static/w8E8hLTXBbTvJUasX93n7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/55a5abef160ffaf9.css","static/chunks/pages/index-b1a8906bcf0844ce.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
