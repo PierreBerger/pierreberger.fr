ab3409f0aba6ba27afb013c9c5413f67511ca8be
Pierre Berger <pierre@fleex.com>
2022-03-04 09:57:51 +0100

    remove Rick Roll

--- pages/wizbii/index.tsx
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react';
-
-export default function Redirect() {
-  useEffect(() => {
-    window.location.assign('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
-  });
-  return <></>;
-}
